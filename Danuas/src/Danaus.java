19038eefed4f4e82402c6ce6568dd2b97ef4c329
booweeeeee <jenniferqian8@gmail.com>
2014-03-14 11:30:49 -0400

    first edit

--- Danuas/src/Danaus.java
+++ Danuas/src/Danaus.java
@@ -1,7 +1,25 @@
+import danaus.Direction;
+import danaus.Speed;
+import danaus.TileState;
+
 
 public class Danaus {
 
-	public int hello(void)
+	public int hello()
 	{
 		return 1;
 	}
+	
+	public TileState[][] learn () {
+		try{
+			fly(danaus.Direction.E, danaus.Speed.NORMAL);
+		}
+		catch (danaus.CliffCollisionException e) {}
+		return null;
+		}
+
+	private void fly(Direction e, Speed normal) {
+		// TODO Auto-generated method stub
+		
+	}
+}
