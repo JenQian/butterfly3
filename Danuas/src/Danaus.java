f6e195aef46533f20f990e913fc7bb4059de39bb
victorfei <vjhfei@gmail.com>
2014-03-14 13:09:28 -0400

    made little changes in fly.java

--- Danuas/src/Danaus.java
+++ Danuas/src/Danaus.java
@@ -1,20 +1,25 @@
-import danaus.Direction;
-import danaus.Speed;
 import danaus.TileState;
 
-public class Danaus 
-{
-	
-	public TileState[][] learn () {
-		try{
-			fly(danaus.Direction.E, danaus.Speed.NORMAL);
-		}
-		catch (danaus.CliffCollisionException e) {}
-		return null;
-		}
 
-	private void fly(Direction e, Speed normal) {
-		// TODO Auto-generated method stub
-		
-	}
+public class Danaus {
+	
+		public TileState[][] learn () 
+		{
+				try{
+					for (;;)
+					{
+						fly(danaus.Direction.E, danaus.Speed.NORMAL);
+					}
+				
+				}
+				catch (danaus.CliffCollisionException e) {}
+				return null;
+			
+		}
+	
+
+//	private void fly(Direction e, Speed normal) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
