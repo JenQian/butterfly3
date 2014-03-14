cd6c32c52bfe3a73c5f1d1258aa1bf264f8d1afc
victorfei <vjhfei@gmail.com>
2014-03-14 17:25:15 -0400

    finished commenting

--- Danuas/src/student/Butterfly.java
+++ Danuas/src/student/Butterfly.java
@@ -20,16 +20,22 @@ public class Butterfly extends AbstractButterfly
 	}
 	 
 	 /* ----------- Helper Methods ------------------------ */
+	 /** Algorithm for the butterfly flying conditions according to the handouts */
 	 private TitleState[][] fly_continuous (Speed s)
 	 {
 		 for (;;)
 		 {
 			 try
 			 {
+				/* Fly the butterfly, at every new tile, it would refresh the 
+				 * state and record the location of the butterfly in its private 
+				 * variables x_position, y_position */
 			 	fly(getDirection(), s);
 			 	refreshState();
 			 	set_location();
 			 	
+			 	/* When the butterfly hits the west edge or east edge of the map
+			 	 * it flies south */
 			 	if ((getx_position() == (getMapWidth()-1)) 
 			 	 || (getx_position() == 0))
 			 	{
@@ -45,9 +51,21 @@ public class Butterfly extends AbstractButterfly
 			 /* For handling different cases when encountering a cliff */
 			 catch (danaus.CliffCollisionException e) 
 			 {
+				/* when the butterfly hits a cliff and its at the south edge of
+				 * the map it terminates the program */
+				 
+				 /*TODO: return new TileState[][], when the butterfly hits a cliff
+				  * and when its at the south edge of the map. This is a terminating
+				  * condition see pg3 of handout. terminating condition 3. 
+				  * so here the program should return a TitleState[][] with all the 
+				  * data that the butterfly traveled on. */
 				if (gety_position() == (getMapHeight()-1))
 					return new TileState[][];
 				
+				/* Handling collision with a cliff:
+				 * Case 1: butterfly flies EAST hits a cliff, it would turn SOUTH
+				 * Case 2: butterfly flies SOUTH hits a cliff, it would turn WEST
+				 * Case 3: butterfly flies WEST hits a cliff, it would turn SOUTH */
 				switch (getDirection())
 				{
 					case E:
@@ -96,12 +114,14 @@ public class Butterfly extends AbstractButterfly
 	 }
 	 
 	 /** @return the direction the butterfly is heading towards */
-	 public Direction getDirection() {
+	 public Direction getDirection() 
+	 {
 		 return direction;
 	 }
 
 	 /** set the direction the butterfly is heading */
-	 public void setDirection(Direction direction) {
+	 public void setDirection(Direction direction) 
+	 {
 		 this.direction = direction;
 	 }
 	 
