30d2d8dcef5fd87c7db800f58c39a9ab9519f393
victorfei <vjhfei@gmail.com>
2014-03-14 16:37:24 -0400

    added situations when the butterfly hits the edge

--- Danuas/src/student/Butterfly.java
+++ Danuas/src/student/Butterfly.java
@@ -7,6 +7,8 @@ import danaus.*;
 public class Butterfly extends AbstractButterfly 
 {
 	private Direction direction = danaus.Direction.E;
+	private int x_position;
+	private int y_position;
 	
 	 public @Override TileState[][] learn() 
 	 {
@@ -26,7 +28,15 @@ public class Butterfly extends AbstractButterfly
 			 {
 			 	fly(getDirection(), s);
 			 	refreshState();
+			 	
+			 	if ((getx_position() == getMapWidth()) || (getx_position() == 0))
+			 	{
+			 		setDirection(danaus.Direction.S);
+			 		fly_continuous(danaus.Speed.NORMAL);
+			 	}
+			 	
 			 }
+			 /* For handling different cases when encountering a cliff */
 			 catch (danaus.CliffCollisionException e) 
 			 {
 				switch (getDirection())
@@ -42,11 +52,28 @@ public class Butterfly extends AbstractButterfly
 						fly_continuous(danaus.Speed.NORMAL);
 						
 					default:
-						break; 
+						setDirection(danaus.Direction.E);
+						fly_continuous(danaus.Speed.NORMAL); 
 				
 				}
 			 }
 		 }
+	 }
+	 
+	 public void set_location()
+	 {
+		 this.x_position = state.location.col;
+		 this.y_position = state.location.row;
+	 }
+	 
+	 public int getx_position()
+	 {
+		 return x_position;
+	 }
+	 
+	 public int gety_position()
+	 {
+		 return y_position;
 	 }
 	 
 	 public Direction getDirection() {
