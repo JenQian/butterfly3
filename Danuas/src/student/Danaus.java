3168b3fb3944b33ec327eadcc1ce81185e2675b7
booweeeeee <jenniferqian8@gmail.com>
2014-03-14 13:09:07 -0400

    try crap  edit

---
+++ Danuas/src/student/Danaus.java
@@ -0,0 +1,39 @@
+package student;
+import java.util.List;
+
+import danaus.*;
+
+public class Danaus extends AbstractButterfly {
+	
+	public TileState[][] learn () {
+		for (int i=0; i<50; i++) {
+			try{
+				fly(danaus.Direction.E, danaus.Speed.NORMAL);
+			}
+			catch (danaus.CliffCollisionException e) {}
+			}
+		return null;
+	}
+	
+	@Override
+	public void run(List<Long> flowerIds) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public List<Flower> flowerList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Location flowerLocation(Flower f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Location flowerLocation(long flowerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+		
+}
