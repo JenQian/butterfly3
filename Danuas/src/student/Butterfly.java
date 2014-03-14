d9bf0cb5896c1cbfbb53b70d9dcaf13246341442
booweeeeee <jenniferqian8@gmail.com>
2014-03-13 22:08:23 -0400

    added gitignore

---
+++ Danuas/src/student/Butterfly.java
@@ -0,0 +1,34 @@
+/** TIME SPENT: # hours and # minutes. */ 
+
+package student;
+import java.util.List;
+import danaus.*;
+
+public class Butterfly extends AbstractButterfly {
+	/**
+	 * Boustrophedonically search the map. 
+	 */
+	public @Override TileState[][] learn() {
+		// TODO: implement
+		return null;
+	}
+	
+	public @Override void run(List<Long> flowerIds) {
+		// DO NOT IMPLEMENT FOR A3
+	}
+	
+	public @Override List<Flower> flowerList() {
+		// DO NOT IMPLEMENT FOR A3
+		return null;
+	}
+	
+	public @Override Location flowerLocation(Flower f) {
+		// DO NOT IMPLEMENT FOR A3
+		return null;
+	}
+	
+	public @Override Location flowerLocation(long flowerId) {
+		// DO NOT IMPLEMENT FOR A3
+		return null;
+	}
+}
